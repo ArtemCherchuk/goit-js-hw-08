09f9b2855b2ffe015f95fee5542890ca7cbb1417
ArtemCherchuk <130292657+ArtemCherchuk@users.noreply.github.com>
2023-08-27 08:20:46 +0200

    fixed task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,45 +1,40 @@
-import _throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
 const FEEDBACK_KEY = 'feedback-form-state';
 
-let feedbackObj = {};
-
-const onSavedMsg = () => {
-  let saveMessage = localStorage.getItem(FEEDBACK_KEY);
-  if (saveMessage) {
-    saveMessage = JSON.parse(saveMessage);
-    Object.entries(saveMessage).forEach(([name, value]) => {
-      formEl.elements[name].value = value;
-    });
-  }
-  return;
-};
-
-onSavedMsg();
+const savFeedback = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
+if (savFeedback) {
+  formEl.elements.email.value = savFeedback.email;
+  formEl.elements.message.value = savFeedback.message;
+}
 
 const onHandleInputForm = event => {
-  // const {
-  //   elements: { email, message },
-  // } = event.currentTarget;
-
-  feedbackObj.email = event.currentTarget.email.value;
-  feedbackObj.message = event.currentTarget.message.value;
-
-  localStorage.setItem(FEEDBACK_KEY, JSON.stringify(feedbackObj));
+  localStorage.setItem(
+    FEEDBACK_KEY,
+    JSON.stringify({
+      email: formEl.elements.email.value,
+      message: formEl.elements.message.value,
+    })
+  );
 };
 
 const onHadleSubmitForm = event => {
   event.preventDefault();
-  saveMessage = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
-  Object.entries(saveMessage).forEach(([email, msg]) => {
-    console.log(email, msg);
-  });
-  event.currentTarget.reset();
-  localStorage.removeItem(FEEDBACK_KEY);
+  const email = formEl.elements.email.value;
+  const msg = formEl.elements.message.value;
+
+  if (email === '' || msg === '') {
+    alert('Error, Fill in all the fields');
+  } else {
+    console.log({
+      email: formEl.elements.email.value,
+      message: formEl.elements.message.value,
+    });
+    event.currentTarget.reset();
+    localStorage.removeItem(FEEDBACK_KEY);
+  }
 };
 
-formEl.addEventListener('input', _throttle(onHandleInputForm, 500));
+formEl.addEventListener('input', throttle(onHandleInputForm, 500));
 formEl.addEventListener('submit', onHadleSubmitForm);
-
-//
